dbadef65b279c8e840e43f3b679f1977eeb365b7
Curiosus43 <curiosus43@gmail.com>
2026-04-12 17:48:41 -0400

    My hacker report acript

---
+++ interação_info.py
@@ -0,0 +1,7 @@
+name = input("Digite seu codinome: ")
+city = input("Localização atual: ")
+tool = input("Ferramenta favorita: ")
+
+print("--- RELATÓRIO DO SISTEMA ---")
+print("O hacker " + name + " acessou o sistema de " + city + " usando " + tool + "!")
+
